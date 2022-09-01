79729ee35645d383f63a9126f2bd159230cd8375
luisbzk <laberrezueta2@protonmail.com>
2022-09-01 16:15:38 -0500

    Routing

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+
+import "./navigation.style.scss";
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to={"/"}>
+          <CrownLogo className="logo" />
+        </Link>
+
+        <div className="nav-links-container">
+          <Link className="nav-link" to={"/shop"}>
+            shop
+          </Link>
+        </div>
+      </div>
+
+      <Outlet />
+    </Fragment>
+  );
+};
+export default Navigation;
